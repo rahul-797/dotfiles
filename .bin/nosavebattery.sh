a580a863ac659afd4a63230f442910d614d2b153
rahul-797 <rahulsangwan797@gmail.com>
2025-04-07 17:17:42 +0530

    added battery save script

---
+++ .bin/nosavebattery.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+hyprctl keyword animations:enabled true > /dev/null
+hyprctl keyword decoration:blur:enabled true > /dev/null
+hyprctl keyword misc:vfr false > /dev/null
---
+++ .bin/savebattery.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+hyprctl keyword animations:enabled false > /dev/null
+hyprctl keyword decoration:blur:enabled false > /dev/null
+hyprctl keyword misc:vfr true > /dev/null
