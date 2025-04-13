30701472ec614e4fba20480ea8020d84a722d0b2
rahul-797 <rahulsangwan797@gmail.com>
2025-04-13 15:19:59 +0530

    clean nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,6 @@
-require("config.settings")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 require("config.lazy")
-require("config.keymap")
-require("after.plugins")
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,11 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  -- enabled = false,
-  -- tag = '0.1.1',
-  branch = '0.1.x',
-  dependencies = { { 'nvim-lua/plenary.nvim' } },
-  -- keys = {
-  --   -- local builtin = require('telescope.builtin'),
-  --   { '<leader>ff', require('telescope.builtin').find_files }
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files)
-    vim.keymap.set("n", "<leader>fk", builtin.keymaps)
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
 }
