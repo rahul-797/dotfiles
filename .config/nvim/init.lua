7eb400cbd96052b3a0185743421e13a9cd87b95c
rahul-797 <rahulsangwan797@gmail.com>
2025-04-14 10:28:21 +0530

    previous nvim restored

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,4 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
+require("config.settings")
 require("config.lazy")
+require("config.keymap")
+require("after.plugins")
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -15,9 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -26,12 +23,31 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "plugins.themes" },
+    { import = "plugins.ui" },
+    { import = "plugins.syntax" },
+    { import = "plugins.lsp" },
+    { import = "plugins.coding" },
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({})
+        end,
+    },
+    {
+        "yetone/avante.nvim",
+        config = function()
+            require("avante").setup()
+        end,
+    },
   },
-  -- Configure any other settings here. See the documentation for more details.
+  ui = {
+    border = "rounded",
+  },
   -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "gruvbox" } },
+  install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- checker = { enabled = true },
 })
-
-require("vim-options")
